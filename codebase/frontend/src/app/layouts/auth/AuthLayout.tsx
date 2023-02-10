68aef6585fc144602139e6944caa992f2aef606d
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 13:20:51 -0300

    feat(authLayout): implement layout

--- codebase/frontend/src/app/layouts/auth/AuthLayout.tsx
+++ codebase/frontend/src/app/layouts/auth/AuthLayout.tsx
@@ -1,11 +1,28 @@
+import { Col, Row } from "@/app/shared/styled";
+import { Typography, useTheme } from "@mui/material";
 import { Outlet } from "react-router";
+import { LeftCol, RightCol, StyledLogo } from "./AuthLayoutStyles";
+import SecureLoginImg from "/secure_login.svg";
+import Logo from "/VERBENA-UFG.png";
 
 const AuthLayout = () => {
+  const theme = useTheme();
+
   return (
     <>
-      <div>AuthLayout</div>
-      <div>AuthLayout</div>
-      <Outlet />
+      <Row>
+        <LeftCol theme={theme}>
+          <Col alignItems="center" mb={10} textAlign="center">
+            <Typography variant="h4">Módulo de Processamento Ágil</Typography>
+            <Typography variant="h5">Instituto Verbena - UFG</Typography>
+          </Col>
+          <img src={SecureLoginImg} alt="Login Ilustration" width="240px" />
+        </LeftCol>
+        <RightCol theme={theme}>
+          <Outlet />
+          <StyledLogo src={Logo} alt="Logo MPA" width="180px" />
+        </RightCol>
+      </Row>
     </>
   );
 };
