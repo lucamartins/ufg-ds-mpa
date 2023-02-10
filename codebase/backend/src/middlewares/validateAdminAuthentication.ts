0748e9f332acf4c8ee9d153461ce3fac9d38dcba
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 14:54:22 -0300

    fix(auth): add bearer keyword to token

--- codebase/backend/src/middlewares/validateAdminAuthentication.ts
+++ codebase/backend/src/middlewares/validateAdminAuthentication.ts
@@ -2,27 +2,36 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 interface IPayload {
-  role: string
+  role: string;
 }
 
-export function validateAdminAuthentication(request: Request, response: Response, next: NextFunction) {
+export function validateAdminAuthentication(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   //Receber o token
-  const authToken = request.headers.authorization;
+  const authToken = String(request.headers.authorization);
 
   //Validar se o token está preenchido
-  if (!authToken) {
-    return response.status(401).json({ message: 'token invalido' });
+  if (!authToken || !authToken.startsWith("Bearer ")) {
+    return response.status(401).json({ message: "token invalido" });
   }
 
   try {
-    const { role } = verify(authToken, process.env.PRIVATE_KEY) as IPayload;
+    const { role } = verify(
+      authToken.substring(7),
+      process.env.PRIVATE_KEY
+    ) as IPayload;
 
-    if (role !== 'ADMIN') {
-      return response.status(401).json({ message: 'usuário não é administrador' });
+    if (role !== "ADMIN") {
+      return response
+        .status(401)
+        .json({ message: "usuário não é administrador" });
     }
 
     return next();
   } catch (err) {
-    return response.status(401).json({ message: 'token invalido' });
+    return response.status(401).json({ message: "token invalido" });
   }
 }
