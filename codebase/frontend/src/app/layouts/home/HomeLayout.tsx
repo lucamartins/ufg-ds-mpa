fdc1aecc49d2001c3dba8dc7ab3b7d72035b7c90
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 18:41:28 -0300

    feat(home): bootstrap acc modal layout and opening/closing logic

--- codebase/frontend/src/app/layouts/home/HomeLayout.tsx
+++ codebase/frontend/src/app/layouts/home/HomeLayout.tsx
@@ -1,17 +1,18 @@
 import { Box, Container } from "@mui/material";
 import { FC } from "react";
 import { Outlet } from "react-router-dom";
-import ResponsiveAppBar from "./AppBar";
+import { AppBar, AccountModal } from "./components";
 
 const HomeLayout: FC = () => {
   return (
     <>
-      <ResponsiveAppBar />
+      <AppBar />
       <Container maxWidth="lg">
         <Box p="25px 12.5px">
           <Outlet />
         </Box>
       </Container>
+      <AccountModal />
     </>
   );
 };
--- codebase/frontend/src/app/shared/stores/appStore.ts
+++ codebase/frontend/src/app/shared/stores/appStore.ts
@@ -7,12 +7,18 @@ interface AppStore {
   snackbarText: string;
   openSnackbar: (text: string, type?: AlertColor) => void;
   closeSnackbar: () => void;
+  accModalOpen: boolean;
+  openAccModal: () => void;
+  closeAccModal: () => void;
 }
 
 const useAppStore = create<AppStore>((set, get) => ({
   isSnackbarOpen: false,
   snackbarType: undefined,
   snackbarText: "",
+  accModalOpen: false,
+  openAccModal: () => set({ accModalOpen: true }),
+  closeAccModal: () => set({ accModalOpen: false }),
   openSnackbar: (text: string, type?: AlertColor) =>
     set({ isSnackbarOpen: true, snackbarType: type, snackbarText: text }),
   closeSnackbar: () => set({ isSnackbarOpen: false }),
