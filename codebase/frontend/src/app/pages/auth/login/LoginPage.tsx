4bc2efe910dea946c406fea1be37b42057ebe305
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 13:33:07 -0300

    feat(loginPage): layout

--- codebase/frontend/src/app/pages/auth/login/LoginPage.tsx
+++ codebase/frontend/src/app/pages/auth/login/LoginPage.tsx
@@ -1,10 +1,25 @@
+import { Col } from "@/app/shared/styled";
+import { Button, TextField, Typography } from "@mui/material";
+
 const LoginPage = () => {
   return (
-    <>
-      <div>LoginPage</div>
-      <div>LoginPage</div>
-      <div>LoginPage</div>
-    </>
+    <Col width="540px">
+      <Col textAlign="start" mb={6}>
+        <Typography variant="h4" mb={1}>
+          Bem vindo!
+        </Typography>
+        <Typography variant="h5" color="light.text.secondary" fontWeight={300}>
+          Insira os detalhes de sua conta para entrar.
+        </Typography>
+      </Col>
+      <Col gap={2} mb={4}>
+        <TextField label="Email" variant="outlined" fullWidth type="email" />
+        <TextField label="Senha" variant="outlined" fullWidth type="password" />
+      </Col>
+      <Button fullWidth variant="contained" size="large">
+        Entrar
+      </Button>
+    </Col>
   );
 };
 
