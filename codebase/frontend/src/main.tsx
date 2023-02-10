f639fffa224de25fa8f6b78d309748a9e7ce34d2
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 17:28:19 -0300

    feat(snackbar): implement custom snackbar global component

--- codebase/frontend/src/main.tsx
+++ codebase/frontend/src/main.tsx
@@ -6,11 +6,13 @@ import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { CustomSnackbar } from "@/app/shared/rootComponents";
 import { router } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
     <RouterProvider router={router} />
+    <CustomSnackbar />
   </React.StrictMode>
 );
