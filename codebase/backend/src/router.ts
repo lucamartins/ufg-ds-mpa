2aa4afee16c0c86d5f6a2ca36586fb8adcfa5728
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 19:40:09 -0300

    refactor(router): add mpa prefix

--- codebase/backend/src/router.ts
+++ codebase/backend/src/router.ts
@@ -1,18 +1,30 @@
-import { Router } from 'express';
-import { validateAuthentication } from './middlewares/validateAuthentication.js';
-import { validateAdminAuthentication } from './middlewares/validateAdminAuthentication.js'
-import { 
+import { Router } from "express";
+import { validateAuthentication } from "./middlewares/validateAuthentication.js";
+import { validateAdminAuthentication } from "./middlewares/validateAdminAuthentication.js";
+import {
   LoginController,
   RegisterUserController,
   UploadCandidatosController,
-  ListProcessesController
-} from './controllers/index.js';
+  ListProcessesController,
+} from "./controllers/index.js";
 
 const router = Router();
 
-router.post('/login', LoginController.handler);
-router.post('/register/user', validateAdminAuthentication, RegisterUserController.handler);
-router.post('/uploads/cadastros', validateAuthentication, UploadCandidatosController.handler);
-router.get('/processes', validateAuthentication, ListProcessesController.handler);
+router.post("/mpa/login", LoginController.handler);
+router.post(
+  "/mpa/register/user",
+  validateAdminAuthentication,
+  RegisterUserController.handler
+);
+router.post(
+  "/mpa/uploads/cadastros",
+  validateAuthentication,
+  UploadCandidatosController.handler
+);
+router.get(
+  "/mpa/processes",
+  validateAuthentication,
+  ListProcessesController.handler
+);
 
 export { router };
