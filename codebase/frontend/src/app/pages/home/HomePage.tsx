ebcbd8b8dffe67403c653c3d846909f64848c8b2
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-10 20:01:37 -0300

    feat(homePage): processes list

--- codebase/frontend/src/app/pages/home/HomePage.tsx
+++ codebase/frontend/src/app/pages/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import {
+  CircularProgress,
   List,
   ListItemButton,
   ListItemIcon,
@@ -7,31 +8,59 @@ import {
 } from "@mui/material";
 import GroupsIcon from "@mui/icons-material/Groups";
 import { useNavigate } from "react-router-dom";
-
-const selectiveProcessItems = ["2021", "2022", "2023"];
+import { useApi } from "@/app/shared/hooks";
+import { useEffect, useState } from "react";
+import { getAllProcessesService } from "@/services";
+import { SelectiveProcess } from "@/services/selective-processes";
+import { Col, Row } from "@/app/shared/styled";
 
 const HomePage = () => {
+  const [selectiveProcesses, setSelectiveProcesses] = useState<
+    SelectiveProcess[] | null
+  >(null);
   const navigate = useNavigate();
+  const api = useApi();
+
+  useEffect(() => {
+    (async () => {
+      const res = await getAllProcessesService(api);
+      setSelectiveProcesses(res.data);
+    })();
+  }, [api]);
 
   return (
     <>
       <Typography variant="h4">Processos Seletivos</Typography>
       <List>
-        {selectiveProcessItems.map((item) => (
-          <ListItemButton
-            onClick={() => {
-              navigate(`details/${item}`);
-            }}
-          >
-            <ListItemIcon key={item}>
-              <GroupsIcon />
-            </ListItemIcon>
-            <ListItemText>
-              PS UFGInclui <b>{item}</b>
-            </ListItemText>
-          </ListItemButton>
-        ))}
+        {selectiveProcesses &&
+          selectiveProcesses.map((process) => (
+            <ListItemButton
+              key={process.id}
+              onClick={() => {
+                navigate(`details/${process.id}`);
+              }}
+            >
+              <ListItemIcon>
+                <GroupsIcon />
+              </ListItemIcon>
+              <ListItemText>
+                PS UFGInclui <b>{process.ano}</b>
+              </ListItemText>
+            </ListItemButton>
+          ))}
       </List>
+      {!selectiveProcesses && (
+        <Col alignItems="center">
+          <CircularProgress />
+        </Col>
+      )}
+      {selectiveProcesses !== null && !selectiveProcesses?.length && (
+        <Row>
+          <Typography variant="h6">
+            Nenhum processo seletivo encontrado.
+          </Typography>
+        </Row>
+      )}
     </>
   );
 };
