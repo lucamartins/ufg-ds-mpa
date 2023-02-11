06ecdcf1ed257dbb3901e342ac762682a82a26ad
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-11 19:39:23 -0300

    feat(settingPage): add analyst service integration and dialog logic

--- codebase/frontend/src/app/pages/settings/SettingsPage.tsx
+++ codebase/frontend/src/app/pages/settings/SettingsPage.tsx
@@ -1,20 +1,32 @@
 import { Row } from "@/app/shared/styled";
 import { Button, Typography } from "@mui/material";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { AddAnalystDialog, AnalystUsersList } from "./components";
 import AddIcon from "@mui/icons-material/Add";
 
 const SettingsPage: FC = () => {
+  const [isAddAnalystDialogOpen, setIsAddAnalystDialogOpen] = useState(false);
+
+  const handleAddAnalystDialogToggle = () => {
+    setIsAddAnalystDialogOpen(!isAddAnalystDialogOpen);
+  };
+
   return (
     <>
       <Row justifyContent="space-between" mb={2}>
         <Typography variant="h5">Acesso ao MPA</Typography>
-        <Button variant="contained" endIcon={<AddIcon />}>
+        <Button
+          variant="contained"
+          endIcon={<AddIcon />}
+          onClick={handleAddAnalystDialogToggle}
+        >
           Cadastrar Analista
         </Button>
       </Row>
       <AnalystUsersList />
-      <AddAnalystDialog />
+      {isAddAnalystDialogOpen && (
+        <AddAnalystDialog handleClose={handleAddAnalystDialogToggle} />
+      )}
     </>
   );
 };
