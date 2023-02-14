b5aa6e913b41a83212044445b1bc4f3eb2fb7da6
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-14 15:15:41 -0300

    fix(deleteAnalyst): param should be sent via url param

--- codebase/backend/src/controllers/DeleteAnalystUserController.ts
+++ codebase/backend/src/controllers/DeleteAnalystUserController.ts
@@ -1,32 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
-import { DeleteUserService } from '../services/DeleteUserService.js';
-import { FindUserById } from '../services/FindUserById.js';
-import { ApiErrors } from '../errors/apiErrors.js';
+import { Request, Response, NextFunction } from "express";
+import { DeleteUserService } from "../services/DeleteUserService.js";
+import { FindUserById } from "../services/FindUserById.js";
+import { ApiErrors } from "../errors/apiErrors.js";
 
 class DeleteAnalystUserController {
-  static async handler(request: Request, response: Response, next: NextFunction) {
+  static async handler(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     const deleteUserService = new DeleteUserService();
     const findUserById = new FindUserById();
 
-    const { id } = request.body;
+    const { id } = request.params;
 
     try {
       const user = await findUserById.execute(id);
 
       if (!user) {
-        throw new ApiErrors(400, 'invalid id');
+        throw new ApiErrors(400, "invalid id");
       }
 
-      if (user.role === 'ADMIN')
-        throw new ApiErrors(401, 'not allowed to delete an amdin user');
+      if (user.role === "ADMIN")
+        throw new ApiErrors(401, "not allowed to delete an amdin user");
 
       await deleteUserService.execute(id);
 
-      return response.status(200).json({message: 'analyst user successfully deleted'});
-    } catch(err) {
+      return response
+        .status(200)
+        .json({ message: "analyst user successfully deleted" });
+    } catch (err) {
       next(err);
     }
   }
 }
 
-export { DeleteAnalystUserController }
+export { DeleteAnalystUserController };
