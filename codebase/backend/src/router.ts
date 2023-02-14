b65cfd7f28562b9a2a88a8cbf45e917082630c57
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-14 15:15:41 -0300

    fix(deleteAnalyst): param should be sent via url param

--- codebase/backend/src/router.ts
+++ codebase/backend/src/router.ts
@@ -1,26 +1,50 @@
-import { Router } from 'express';
-import { validateAuthentication } from './middlewares/validateAuthentication.js';
-import { validateAdminAuthentication } from './middlewares/validateAdminAuthentication.js';
-import { 
+import { Router } from "express";
+import { validateAuthentication } from "./middlewares/validateAuthentication.js";
+import { validateAdminAuthentication } from "./middlewares/validateAdminAuthentication.js";
+import {
   LoginController,
   RegisterUserController,
   ListProcessesController,
   RegisterProcessController,
   UploadCandidatosController,
   ListAnalystUsersController,
-  DeleteAnalystUserController
-} from './controllers/index.js';
+  DeleteAnalystUserController,
+} from "./controllers/index.js";
 
 const router = Router();
 
-router.get('/mpa/processes', validateAuthentication, ListProcessesController.handler);
-router.get('/mpa/users/analyst', validateAuthentication, ListAnalystUsersController.handler);
+router.get(
+  "/mpa/processes",
+  validateAuthentication,
+  ListProcessesController.handler
+);
+router.get(
+  "/mpa/users/analyst",
+  validateAuthentication,
+  ListAnalystUsersController.handler
+);
 
-router.post('/mpa/login', LoginController.handler);
-router.post('/mpa/register/user', validateAdminAuthentication, RegisterUserController.handler);
-router.post('/mpa/uploads/cadastros', validateAuthentication, UploadCandidatosController.handler);
-router.post('/mpa/register/process', validateAdminAuthentication, RegisterProcessController.handler);
+router.post("/mpa/login", LoginController.handler);
+router.post(
+  "/mpa/register/user",
+  validateAdminAuthentication,
+  RegisterUserController.handler
+);
+router.post(
+  "/mpa/uploads/cadastros",
+  validateAuthentication,
+  UploadCandidatosController.handler
+);
+router.post(
+  "/mpa/register/process",
+  validateAdminAuthentication,
+  RegisterProcessController.handler
+);
 
-router.delete('/mpa/users/analyst', validateAdminAuthentication, DeleteAnalystUserController.handler);
+router.delete(
+  "/mpa/users/analyst/:id",
+  validateAdminAuthentication,
+  DeleteAnalystUserController.handler
+);
 
 export { router };
