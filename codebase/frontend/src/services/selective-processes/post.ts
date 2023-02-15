413b3bb3781fe5c5bf308f039506c90b622d9424
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-15 15:36:37 -0300

    feat(services): first step service

--- codebase/frontend/src/services/selective-processes/post.ts
+++ codebase/frontend/src/services/selective-processes/post.ts
@@ -1,7 +1,12 @@
 import { ApiType } from "../http";
-import { AddProcessReqData } from "./post.types";
+import { AddProcessReqData, ProcessFirstStepReqData } from "./post.types";
 
 export const addProcessService = async (
   api: ApiType,
   data: AddProcessReqData
 ) => await api.post("/register/process", data);
+
+export const uploadFirstStepService = async (
+  api: ApiType,
+  data: ProcessFirstStepReqData
+) => await api.post("/uploads/cadastros", data);
