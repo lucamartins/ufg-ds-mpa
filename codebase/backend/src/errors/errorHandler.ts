b8872dbf09f2fbc7651b09c72e63636b154f3490
Luca Santos Martins <lucamartins.dev@gmail.com>
2023-02-22 02:28:48 -0300

    log errors to console

--- codebase/backend/src/errors/errorHandler.ts
+++ codebase/backend/src/errors/errorHandler.ts
@@ -1,15 +1,22 @@
 import { NextFunction, Request, Response, Errback } from "express";
-import { ApiErrors } from './apiErrors.js';
+import { ApiErrors } from "./apiErrors.js";
 
-export function apiErrorHandler(err: Errback, request: Request, response: Response, next: NextFunction) {
+export function apiErrorHandler(
+  err: Errback,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  console.log(err);
+
   if (err instanceof ApiErrors) {
     return response.status(err.code).json({
-      error: err.message
+      error: err.message,
     });
   }
 
   return response.status(500).json({
     status: "error",
-    message: "Internal Server Error"
+    message: "Internal Server Error",
   });
 }
